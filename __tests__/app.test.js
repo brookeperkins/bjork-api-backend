9a155cbf2bf656f413aed54b32d5a9ff043bf28e
brookeperkins <brookemmperkins@gmail.com>
2020-11-01 18:23:24 -0800

    ahhh

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,9 +9,9 @@ describe('bjork-api-be routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  it('creates a new bjork outfit entry', async() => {
+  it('creates a new bjork outfit entry', () => {
     return request(app)
-      .post('/api/outfits')
+      .post('/outfits')
       .send({
         img: 'bjork.jpg',
         year: 1995,
@@ -45,11 +45,73 @@ describe('bjork-api-be routes', () => {
     }].map(outfit => Outfit.insert(outfit)));
 
     return request(app)
-      .get('/api/outfits')
+      .get('/outfits')
       .then(res => {
         outfits.forEach(outfit => {
           expect(res.body).toContainEqual(outfit);
         });
       });
+  });
+
+  it('gets an entry by id', async() => {
+    const outfit = await Outfit.insert({
+      img: 'Bjork1.jpg',
+      year: 1994,
+      quote: 'bjork quote',
+    });
+
+    return request(app)
+      .get(`/outfits/${outfit.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          img: 'Bjork1.jpg',
+          year: 1994,
+          quote: 'bjork quote'
+        });
+      });
+  });
+
+  it('updates an entry by id', async() => {
+    const outfit = await Outfit.insert({
+      img: 'BjorkPic.jpg',
+      year: '2016',
+      quote: 'Quote quote quote.',
+    });
+
+    return request(app)
+      .put(`/outfits/${outfit.id}`)
+      .send({
+        img: 'NewPic.jpg',
+        year: 2011,
+        quote: 'Here is a quote!',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          img: 'NewPic.jpg',
+          year: 2011,
+          quote: 'Here is a quote!'
+        });
+      });
+  });
+
+  it('deletes an entry by id', async() => {
+    const outfit = await Outfit.insert({
+      img: 'Bjork.png',
+      year: 1999,
+      quote: 'Hi, my name is Bjork.'
+    });
+
+    return request(app)
+      .delete(`/sailors/${outfit.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          img: 'Bjork.png',
+          year: 1999,
+          quote: 'Hi, my name is Bjork.'
+        });
+      }); 
   });
 });
